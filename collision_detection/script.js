fa88b66e07a208c0bd9565ae3f99de22eff862c1
krpinkney <15308389@massey.ac.nz>
2016-05-02 11:01:34 +1200

    Booleans & Collision game WIP

---
+++ collision_detection/script.js
@@ -0,0 +1,16 @@
+var puck_x = 210;
+var puck_y = 210;
+
+document.getElementById ('puck').style.top = puck_y + 'px';
+document.getElementById ('puck').style.left = puck_x + 'px';
+/*
+
+Begin by following the steps in the workshop notes.
+
+Next, add the following features:
+- the gap goes red when the puck is over it
+- the puck can't go beyond the arena borders
+- create an item that makes the puck bigger
+- create an item that makes the puck smaller
+
+*/
