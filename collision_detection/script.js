9297e2e0d2b60e5cdcc2ee619f2cd44bd04ec6fb
krpinkney <15308389@massey.ac.nz>
2016-05-02 11:48:44 +1200

    Collision Detector Game WIP

--- collision_detection/script.js
+++ collision_detection/script.js
@@ -1,8 +1,49 @@
 var puck_x = 210;
 var puck_y = 210;
+var speed = 50;
 
 document.getElementById ('puck').style.top = puck_y + 'px';
 document.getElementById ('puck').style.left = puck_x + 'px';
+
+function logCoordinates() {
+    console.log('x:'+ puck_x +' y:' + puck_y);
+}
+
+function checkCollision() {
+    if (puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250) {
+        console.log('collision detected');
+    }
+}
+
+document.getElementById ('right').addEventListener('click', function () {
+    puck_x += speed;
+    document.getElementById ('puck').style.left = puck_x + 'px';
+    logCoordinates();
+    checkCollision();
+});
+
+
+document.getElementById ('left').addEventListener('click', function () {
+    puck_x -= speed;
+    document.getElementById ('puck').style.left = puck_x + 'px';
+    logCoordinates();
+    checkCollision();
+});
+
+document.getElementById ('up').addEventListener('click', function () {
+    puck_y -= speed;
+    document.getElementById ('puck').style.top = puck_y + 'px';
+    logCoordinates();
+    checkCollision();
+});
+
+document.getElementById ('down').addEventListener('click', function () {
+    puck_y += speed;
+    document.getElementById ('puck').style.top = puck_y + 'px';
+    logCoordinates();
+    checkCollision();
+});
+
 /*
 
 Begin by following the steps in the workshop notes.
