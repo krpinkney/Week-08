1302b94d0c120a9022152d2598ec754b73f355c0
krpinkney <Kristina@Atticus.local>
2016-05-08 21:37:46 +1200

    Collision Detector WIP

--- collision_detection/script.js
+++ collision_detection/script.js
@@ -1,6 +1,6 @@
-var puck_x = 210;
-var puck_y = 210;
-var speed = 50;
+var puck_x = 200;
+var puck_y = 200;
+var speed = 25;
 
 document.getElementById ('puck').style.top = puck_y + 'px';
 document.getElementById ('puck').style.left = puck_x + 'px';
@@ -12,6 +12,21 @@ function logCoordinates() {
 function checkCollision() {
     if (puck_x > 250 && puck_x < 350 && puck_y > 150 && puck_y < 250) {
         console.log('collision detected');
+        document.getElementById('gap').style.backgroundColor = 'red';
+    } else {
+        document.getElementById('gap').style.backgroundColor = 'white';
+    }
+}
+
+function checkBoundaries() {
+    if (puck_x >= 550 ) {
+        puck_x = 550;
+    } else if (puck_x <= 25 ) {
+        puck_x = 25;
+    } else if (puck_y >= 350 ) {
+        puck_y = 350;
+    } else if (puck_y <= 25 ) {
+        puck_y = 25;
     }
 }
 
@@ -20,6 +35,7 @@ document.getElementById ('right').addEventListener('click', function () {
     document.getElementById ('puck').style.left = puck_x + 'px';
     logCoordinates();
     checkCollision();
+    checkBoundaries();
 });
 
 
@@ -28,6 +44,7 @@ document.getElementById ('left').addEventListener('click', function () {
     document.getElementById ('puck').style.left = puck_x + 'px';
     logCoordinates();
     checkCollision();
+    checkBoundaries();
 });
 
 document.getElementById ('up').addEventListener('click', function () {
@@ -35,6 +52,7 @@ document.getElementById ('up').addEventListener('click', function () {
     document.getElementById ('puck').style.top = puck_y + 'px';
     logCoordinates();
     checkCollision();
+    checkBoundaries();
 });
 
 document.getElementById ('down').addEventListener('click', function () {
@@ -42,6 +60,7 @@ document.getElementById ('down').addEventListener('click', function () {
     document.getElementById ('puck').style.top = puck_y + 'px';
     logCoordinates();
     checkCollision();
+    checkBoundaries();
 });
 
 /*
@@ -49,9 +68,9 @@ document.getElementById ('down').addEventListener('click', function () {
 Begin by following the steps in the workshop notes.
 
 Next, add the following features:
-- the gap goes red when the puck is over it
-- the puck can't go beyond the arena borders
-- create an item that makes the puck bigger
+- the gap goes red when the puck is over it /
+- the puck can't go beyond the arena borders /
+- create an item that makes the puck bigger 
 - create an item that makes the puck smaller
 
 */
